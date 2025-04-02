e19b24ab49c4da771bf84c60b61ca0139baf2f97
kmg7 <kmlgkcy.dev@gmail.com>
2025-04-02 16:22:53 +0300

    feat: implement ValueObject base class

---
+++ pkg/kmg7.dodo.SharedKernel/kmg7.dodo.SharedKernel/Framework/ValueObject.cs
@@ -0,0 +1,119 @@
+﻿namespace kmg7.dodo.SharedKernel.Framework;
+
+// Credits to Vladimir: https://enterprisecraftsmanship.com/posts/value-object-better-implementation/
+
+[Serializable]
+public abstract class ValueObject : IComparable, IComparable<ValueObject>
+{
+    private int? _cachedHashCode;
+
+    protected abstract IEnumerable<object> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null)
+            return false;
+
+        if (GetTypeWithoutProxy(this) != GetTypeWithoutProxy(obj))
+            return false;
+
+        var valueObject = (ValueObject)obj;
+
+        return GetEqualityComponents().SequenceEqual(valueObject.GetEqualityComponents());
+    }
+
+    public override int GetHashCode()
+    {
+        if (!_cachedHashCode.HasValue)
+        {
+            _cachedHashCode = GetEqualityComponents()
+                .Aggregate(1, (current, obj) =>
+                {
+                    unchecked
+                    {
+                        return current * 23 + (obj?.GetHashCode() ?? 0);
+                    }
+                });
+        }
+
+        return _cachedHashCode.Value;
+    }
+
+    public int CompareTo(object? obj)
+    {
+        if (obj == null)
+            return 1;
+
+        var thisType = GetTypeWithoutProxy(this);
+        var otherType = GetTypeWithoutProxy(obj);
+
+        if (thisType != otherType)
+            return string.Compare(thisType.ToString(), otherType.ToString(), StringComparison.Ordinal);
+
+        var other = (ValueObject)obj;
+
+        var components = GetEqualityComponents().ToArray();
+        var otherComponents = other.GetEqualityComponents().ToArray();
+
+        for (var i = 0; i < components.Length; i++)
+        {
+            var comparison = CompareComponents(components[i], otherComponents[i]);
+            if (comparison != 0)
+                return comparison;
+        }
+
+        return 0;
+    }
+
+    private static int CompareComponents(object? object1, object? object2)
+    {
+        if (object1 is null && object2 is null)
+            return 0;
+
+        if (object1 is null)
+            return -1;
+
+        if (object2 is null)
+            return 1;
+
+        if (object1 is IComparable comparable1 && object2 is IComparable comparable2)
+            return comparable1.CompareTo(comparable2);
+
+        return object1.Equals(object2) ? 0 : -1;
+    }
+
+    public int CompareTo(ValueObject? other)
+    {
+        return CompareTo(other as object);
+    }
+
+    public static bool operator ==(ValueObject left, ValueObject right)
+    {
+        if (left is null && right is null)
+            return true;
+
+        if (left is null || right is null)
+            return false;
+
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(ValueObject left, ValueObject right)
+    {
+        return !(left == right);
+    }
+
+    internal static Type GetTypeWithoutProxy(object obj)
+    {
+        ArgumentNullException.ThrowIfNull(obj);
+
+        const string EFCoreProxyPrefix = "Castle.Proxies.";
+
+        var type = obj.GetType();
+
+        if (type.ToString().Contains(EFCoreProxyPrefix))
+            return type.BaseType!;
+
+        return type;
+    }
+}
