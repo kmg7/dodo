efc0a68fd91be026399b9b80297b2c3e0cbf7bc1
kmg7 <kmlgkcy.dev@gmail.com>
2025-04-02 16:23:04 +0300

    feat: add unit tests for ValueObject equality and hash code consistency

---
+++ pkg/kmg7.dodo.SharedKernel/kmg7.dodo.SharedKernel.Tests/Framework/TestValueObject.cs
@@ -0,0 +1,23 @@
+﻿using kmg7.dodo.SharedKernel.Framework;
+
+namespace kmg7.dodo.SharedKernel.Tests.Framework;
+public class TestValueObject : ValueObject
+{
+    public int IntegerValue { get; }
+    public List<string> Strings { get; }
+
+    public TestValueObject(int integerValue, List<string> strings)
+    {
+        IntegerValue = integerValue;
+        Strings = strings;
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return IntegerValue;
+        foreach (var item in Strings)
+        {
+            yield return item;
+        }
+    }
+}
---
+++ pkg/kmg7.dodo.SharedKernel/kmg7.dodo.SharedKernel.Tests/Framework/ValueObjectTests.cs
@@ -0,0 +1,51 @@
+﻿using Shouldly;
+
+namespace kmg7.dodo.SharedKernel.Tests.Framework;
+public class ValueObjectTests
+{
+    [Fact]
+    public void ValueObjects_with_same_values_should_be_equal()
+    {
+        var valueObject1 = new TestValueObject(1, ["a"]);
+        var valueObject2 = new TestValueObject(1, ["a"]);
+
+        valueObject1.ShouldBe(valueObject2);
+    }
+
+    [Theory]
+    [InlineData(1, "a", "b", 2, "a", "b")]
+    [InlineData(2, "a", "b", 2, "b", "a")]
+    public void ValueObjects_with_different_values_should_not_be_equal(int numA, string textA1, string textA2, int numbB, string textB1, string textB2)
+    {
+        var valueObject1 = new TestValueObject(numA, [textA1, textA2]);
+        var valueObject2 = new TestValueObject(numbB, [textB1, textB2]);
+
+        valueObject1.ShouldNotBe(valueObject2);
+    }
+
+    [Fact]
+    public void ValueObjects_with_same_values_should_have_equal_hash_codes()
+    {
+        var valueObject1 = new TestValueObject(1, ["a"]);
+        var valueObject2 = new TestValueObject(1, ["a"]);
+
+        var valueObject1HashCode = valueObject1.GetHashCode();
+        var valueObject2HashCode = valueObject2.GetHashCode();
+
+        valueObject1HashCode.ShouldBe(valueObject2HashCode);
+    }
+
+    [Theory]
+    [InlineData(1, "a", "b", 2, "a", "b")]
+    [InlineData(2, "a", "b", 2, "b", "a")]
+    public void ValueObjects_with_different_values_should_not_have_equal_hash_codes(int numA, string textA1, string textA2, int numbB, string textB1, string textB2)
+    {
+        var valueObject1 = new TestValueObject(numA, [textA1, textA2]);
+        var valueObject2 = new TestValueObject(numbB, [textB1, textB2]);
+
+        var valueObject1HashCode = valueObject1.GetHashCode();
+        var valueObject2HashCode = valueObject2.GetHashCode();
+
+        valueObject1HashCode.ShouldNotBe(valueObject2HashCode);
+    }
+}
