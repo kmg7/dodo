fc1c357fc59754e47b78ff6c064e3663b94a8855
kmg7 <kmlgkcy.dev@gmail.com>
2025-07-18 15:18:37 +0300

    feat: add commands and TaskService for task management operations

---
+++ apps/task-service/kmg7.dodo.TaskManagementContext.Application/Commands.cs
@@ -0,0 +1,7 @@
+﻿namespace kmg7.dodo.TaskManagementContext.Application;
+public static class Commands
+{
+    public record CreateTaskCommand(Guid UserId, string Title, string? Description);
+    public record UpdateTaskCommand(Guid UserId, Guid TaskId, string Title, string? Description);
+    public record CompleteTaskCommand(Guid UserId, Guid TaskId);
+}
---
+++ apps/task-service/kmg7.dodo.TaskManagementContext.Application/TaskService.cs
@@ -0,0 +1,17 @@
+﻿using kmg7.dodo.TaskManagementContext.Domain.TaskAggregate;
+
+namespace kmg7.dodo.TaskManagementContext.Application;
+
+public class TaskService
+{
+    // ITaskAggregateRepository _taskAggregateRepository;
+    public void CreateTask(Commands.CreateTaskCommand c)
+    {
+        // Create a new task
+        // start unit of work
+        DomainTask task = DomainTask.Create(c.UserId, c.Title, c.Description);
+        // Try to save the task to persistence
+        // commit
+        // if successful client can use the query model to get the task
+    }
+}
