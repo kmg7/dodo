791e24c900d933550d461341e1351d5659e1a6a7
kmg7 <kmlgkcy.dev@gmail.com>
2025-07-18 15:18:45 +0300

    feat: add test project and initial tests for DomainTask functionality

---
+++ apps/task-service/kmg7.dodo.TaskManagementContext.Tests/TaskTests.cs
@@ -0,0 +1,33 @@
+﻿namespace kmg7.dodo.TaskManagementContext.Tests;
+
+public class TaskTests
+{
+    readonly Domain.TaskAggregate.DomainTask _sut;
+    public TaskTests()
+    {
+        _sut = TestTask();
+    }
+
+    public Domain.TaskAggregate.DomainTask TestTask()
+    {
+        var ownerId = Guid.CreateVersion7();
+        var title = "Test Task";
+        var description = "This is a test task.";
+
+        return Domain.TaskAggregate.DomainTask.Create(ownerId, title, description);
+    }
+
+    [Fact]
+    public void Task_should()
+    {
+        var num1 = 1;
+        try
+        {
+            num1.ShouldBeGreaterThan(5);
+        }
+        catch (Exception)
+        {
+
+        }
+    }
+}
