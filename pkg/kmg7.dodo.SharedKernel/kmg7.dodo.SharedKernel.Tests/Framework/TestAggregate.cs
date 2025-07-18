27bad48590934372bfbc764730b46ede18ddb827
kmg7 <kmlgkcy.dev@gmail.com>
2025-07-18 15:18:10 +0300

    feat: add TestAggregate and TestEntity classes with domain event handling

---
+++ pkg/kmg7.dodo.SharedKernel/kmg7.dodo.SharedKernel.Tests/Framework/TestAggregate.cs
@@ -0,0 +1,33 @@
+﻿using kmg7.dodo.SharedKernel.Framework;
+
+namespace kmg7.dodo.SharedKernel.Tests.Framework;
+public class TestAggregate : AggregateRoot<TestAggregateId>
+{
+    public TestValueObject ValueA { get; set; }
+    public List<TestEntity> Entities { get; set; } = [];
+
+    protected override void EnsureValidState()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void Mutate(IDomainEvent @event)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+
+public class TestAggregateId : ValueObject
+{
+    public int Id { get; }
+    public TestAggregateId(int id)
+    {
+        Id = id;
+    }
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Id;
+    }
+}
---
+++ pkg/kmg7.dodo.SharedKernel/kmg7.dodo.SharedKernel.Tests/Framework/TestEntity.cs
@@ -0,0 +1,62 @@
+﻿using kmg7.dodo.SharedKernel.Framework;
+
+namespace kmg7.dodo.SharedKernel.Tests.Framework;
+public class TestEntity : Entity<TestEntityId>
+{
+    public string Name { get; private set; }
+    public int MyProperty { get; set; }
+    public TestEntity(TestEntityId id, string name, Action<IDomainEvent> eventApplier) : base(eventApplier)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public void ChangeName(string newName)
+    {
+        var @event = new TestEntityNameChangedEvent(DateTime.UtcNow, newName);
+        ApplyEvent(@event);
+    }
+
+    protected override void Mutate(IDomainEvent @event)
+    {
+        switch (@event)
+        {
+            case TestEntityNameChangedEvent nameChangedEvent:
+                When(nameChangedEvent);
+                break;
+            default:
+                throw new ArgumentException($"Unknown event type: {@event.GetType()}");
+        }
+    }
+
+    private void When(TestEntityNameChangedEvent @event)
+    {
+        Name = @event.NewName;
+    }
+}
+
+public class TestEntityNameChangedEvent : IDomainEvent
+{
+    public DateTime OccurredOn { get; }
+    public int Version { get; } = 1;
+    public string NewName { get; }
+    public TestEntityNameChangedEvent(DateTime occurredOn, string newName)
+    {
+        OccurredOn = occurredOn;
+        NewName = newName;
+    }
+}
+
+
+public class TestEntityId : ValueObject
+{
+    public int Id { get; }
+    public TestEntityId(int id)
+    {
+        Id = id;
+    }
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Id;
+    }
+}
